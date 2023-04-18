b0aa5935458ff6918737c28c215df92177badea1
Ramzi <ramzi.daher@gmail.com>
2023-04-18 11:02:50 +0100

    stops window from being resized for the mac hopefully

--- src/main/java/com/settlers/demotesty/Main.java
+++ src/main/java/com/settlers/demotesty/Main.java
@@ -4,10 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 
-public class
-Main extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 //        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("WelcomePage.fxml"));
@@ -15,6 +15,10 @@ Main extends Application {
 //        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("SignUpPage.fxml"));
 
         Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
+        if (isMacOS()) {
+            scene.getRoot().setScaleX(0.75);
+            scene.getRoot().setScaleY(0.75);
+        }
         stage.setTitle("Welcome To Settlers!!!");
         stage.setScene(scene);
         stage.setResizable(false); // Prevent the window from being resized
@@ -22,7 +26,10 @@ Main extends Application {
     }
 
     public static void main(String[] args) {
-        System.setProperty("prism.allowhidpi", "false"); // Disable HiDPI scaling for JavaFX
         launch();
+    }
+
+    private static boolean isMacOS() {
+        return System.getProperty("os.name").toLowerCase().contains("mac");
     }
 }
