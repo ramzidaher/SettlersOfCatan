d3f54664cc3ab306487c6d3dcc742e6dfd5f6772
Ramzi <ramzi.daher@gmail.com>
2023-04-18 10:33:29 +0100

    stops window from being resized

--- src/main/java/com/settlers/demotesty/Main.java
+++ src/main/java/com/settlers/demotesty/Main.java
@@ -6,17 +6,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 
-public class
-Main extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("WelcomePage.fxml"));
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("test.fxml"));
-//        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("SignUpPage.fxml"));
-
         Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
         stage.setTitle("Welcome To Settlers!!!");
         stage.setScene(scene);
+        stage.setResizable(false); // set resizable property to false
         stage.show();
     }
 
